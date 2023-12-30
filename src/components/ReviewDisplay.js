bfcfe901080743d923602c6ae55aa8b9db93c735
Muzna-J <muzna.jabban@gmail.com>
2023-12-30 13:59:09 +0100

    delete review

--- src/components/ReviewDisplay.js
+++ src/components/ReviewDisplay.js
@@ -5,6 +5,15 @@ const ReviewDisplay = ({volumeId}) => {
     const [reviews, setReviews] = useState([]);
     const [noReviewsMessage, setNoReviewsMessage] = useState('');
 
+    const deleteReview = async (reviewId) => {
+        try {
+            await axios.delete(`http://localhost:5005/delete-review/${reviewId}`, { withCredentials: true });
+            setReviews(reviews.filter(review => review._id !== reviewId));
+        } catch (error) {
+            console.error('Error deleting review', error);
+        }
+    };
+
     useEffect(() => {
         const fetchReviews = async () => {
             try{
@@ -32,6 +41,7 @@ const ReviewDisplay = ({volumeId}) => {
                         <div>User: {review.user.name}</div>
                         <div>Rating: {review.rating} stars</div>
                         <div>Comment: {review.comment}</div>
+                        <button onClick={() => deleteReview(review._id)}>Delete Review</button>
                     </div>
                 ))
             )}
