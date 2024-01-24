1ab280ce319dbfdbc5d96ff34ef57f4d9db20141
Muzna-J <muzna.jabban@gmail.com>
2024-01-24 02:50:45 +0100

    fix form visibility cancel editing

--- src/components/ReviewDisplay.js
+++ src/components/ReviewDisplay.js
@@ -5,131 +5,90 @@ import { UserContext } from '../context/UserContext';
 import StarRatingDisplay from './StarRatingDisplay';
 import { toast } from 'react-toastify';
 
-const ReviewDisplay = ({volumeId, triggerRefresh}) => {
+const ReviewDisplay = ({ volumeId, triggerRefresh }) => {
     const [reviews, setReviews] = useState([]);
     const [noReviewsMessage, setNoReviewsMessage] = useState('');
     const [editingReview, setEditingReview] = useState(null);
-    const [showForm, setShowForm] = useState(false);
+    const [showForm, setShowForm] = useState(true);  // Initially show the form
     const { currentUser } = useContext(UserContext);
-    
 
     const startEditing = (review) => {
         setEditingReview(review);
-        if (!showForm) {
-            setShowForm(true);
-        }  
+        setShowForm(true);  // Ensure form is visible when editing
     };
 
     const handleSubmissionSuccess = () => {
-        setShowForm(false); 
-        triggerRefresh(); 
-        
-
-    };
-
-    const hideForm = () => {
-        //setEditingReview(null);
-        setShowForm(false);
+        setShowForm(false);  // Hide form after successful submission
+        triggerRefresh();
     };
 
     const deleteReview = async (reviewId) => {
         try {
             await axios.delete(`http://localhost:5005/delete-review/${reviewId}`, { withCredentials: true });
             setReviews(reviews.filter(review => review._id !== reviewId));
+            toast.success('Review deleted!');
+            triggerRefresh();
         } catch (error) {
             console.error('Error deleting review', error);
         }
     };
-
-    const handleRefresh = () => {
-        triggerRefresh();
-        hideForm();
+    const cancelEditing = () => {
+        setEditingReview(null);
+        setShowForm(false);
     };
-
-    const handleAddReviewClick = () => {
-        setEditingReview(null); // Ensure no review is being edited
-        setShowForm(true);      // Show the form
-    };
-
-    
 
     useEffect(() => {
         const fetchReviews = async () => {
-            try{
-                const response = await axios.get(`http://localhost:5005/get-reviews/${volumeId}`, {withCredentials: true});
-                if (response.data.message) {
-                    // Handle the case where there are no reviews
-                    setNoReviewsMessage(response.data.message);
-                  } else {
-                setReviews(response.data)};
-                setNoReviewsMessage(''); // Reset message in case of actual reviews
+            try {
+                const response = await axios.get(`http://localhost:5005/get-reviews/${volumeId}`, { withCredentials: true });
+                if (!response.data.message) {
+                    setReviews(response.data);
+                    const userHasReviewed = response.data.some(review => review.user._id === currentUser?.id);
+                    setShowForm(!userHasReviewed);  // Hide form if user has reviewed
+                }
             } catch (error) {
-                console.error('Error fetching reviews', error)
+                console.error('Error fetching reviews', error);
             }
         };
         fetchReviews();
-    }, [volumeId, triggerRefresh]);
+    }, [volumeId, triggerRefresh, currentUser?.id]);
 
     return (
         <div className='my-4'>
-        
-        {/* <button onClick={handleAddReviewClick} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mb-4">
-                Add Review
-            </button> */}
-
             {showForm && (
                 <ReviewForm
                     volumeId={volumeId}
                     existingReview={editingReview}
-                    triggerRefresh={handleRefresh}
-                    hideForm={hideForm}
+                    triggerRefresh={triggerRefresh}
+                    hideForm={() => setShowForm(false)}
                     onSubmitSuccess={handleSubmissionSuccess}
+                    cancelEditing={cancelEditing}
                 />
             )}
-            <h2 className="text-xl font-bold mb-4">Reviews</h2>
-            {!showForm && (
-                <button onClick={handleAddReviewClick} className="your-button-styles">
-                    Add Review
-                </button>
-            )}
-           
-        
-        {reviews.length === 0 ? (
-            <p className='text-gray-600 italic'>No reviews yet.</p>
-        ) : (
-                
-                reviews.map((review, index) => {
-                    const isCurrentUser = currentUser && review.user._id === currentUser.id;
-                    console.log("Current User ID:", currentUser?.id, "Review User ID:", review.user._id, "Is Current User:", isCurrentUser);
-                    // Log the information outside of the JSX, but inside the map callback
-                    //  console.log("Review User ID:", review.user, "Type:", typeof review.user);
-    
-                    // Return the JSX explicitly
-                    return (
-                        <div key={index} className='bg-white p-4 rounded-lg shadow-md mb-4 max-w-xl'>
-                            <div className='font-bold'>{review.user.name}</div>
-                            <StarRatingDisplay rating={review.rating} />
-                            <p className='text-gray-700'>{review.comment}</p>
 
-                            {isCurrentUser && (
-                                <div className="flex justify-center mt-2">
-    
-                                    <button onClick={() => startEditing(review)} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded mr-2'>Edit Review</button>
-                                    <button onClick={() => deleteReview(review._id)} className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-3 rounded">Delete Review</button>
-                        </div>
-                    )};
+            <h2 className="text-xl font-bold mb-4">Reviews</h2>
+        
+            {reviews.length === 0 ? (
+                <p className='text-gray-600 italic'>{noReviewsMessage || 'No reviews yet.'}</p>
+            ) : (
+                reviews.map((review, index) => (
+                    <div key={index} className='bg-white p-4 rounded-lg shadow-md mb-4 max-w-xl'>
+                        <div className='font-bold'>{review.user.name}</div>
+                        <StarRatingDisplay rating={review.rating} />
+                        <p className='text-gray-700'>{review.comment}</p>
+
+                        {currentUser && review.user._id === currentUser.id && (
+                            <div className="flex justify-center mt-2">
+                                <button onClick={() => startEditing(review)} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded mr-2'>Edit Review</button>
+                                <button onClick={() => deleteReview(review._id)} className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-3 rounded">Delete Review</button>
+                            </div>
+                        )}
                     </div>
-                );
-                })
+                ))
             )}
-    
-       
         </div>
     );
-    
- }
+}
+
 export default ReviewDisplay;
-
-
-
 
--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -5,7 +5,7 @@ import { toast } from 'react-toastify';
 
 
 
-const ReviewForm = ({ volumeId, existingReview, triggerRefresh, hideForm, onSubmitSuccess }) => {
+const ReviewForm = ({ volumeId, existingReview, triggerRefresh, hideForm, onSubmitSuccess, cancelEditing }) => {
     const [rating, setRating] = useState(0);
     const [comment, setComment] = useState('');
     const [isEditing, setIsEditing] = useState(false);
@@ -21,57 +21,24 @@ const ReviewForm = ({ volumeId, existingReview, triggerRefresh, hideForm, onSubm
     }, [existingReview]);
 
 
-    // const handleSubmit= async (e) => {
-    //     e.preventDefault();
-    //     const url = isEditing ? `http://localhost:5005/edit-review/${existingReview._id}` : 'http://localhost:5005/add-review';
-    //     const method = isEditing ? axios.put : axios.post;
-    //     try {
-    //         await method(url, { rating, comment, volumeId }, { withCredentials: true });
-    //         triggerRefresh();
-    //         hideForm();
-    //         onSubmitSuccess();
-    //         toast.success(isEditing ? 'Review updated successfully!' : 'Review added successfully!');
-           
-            
-    //     } catch (error) {
-    //         console.error('Error submitting review', error.response);
-    //         toast.error("Error submitting review.");
-    //     }
-    // };
-
-
-    const handleSubmit = async (e) => {
+    const handleSubmit= async (e) => {
         e.preventDefault();
         const url = isEditing ? `http://localhost:5005/edit-review/${existingReview._id}` : 'http://localhost:5005/add-review';
         const method = isEditing ? axios.put : axios.post;
-    
         try {
-            const response = await method(url, { rating, comment, volumeId }, { withCredentials: true });
-            console.log('Response:', response); // Log the response for debugging
+            await method(url, { rating, comment, volumeId }, { withCredentials: true });
             triggerRefresh();
             hideForm();
             onSubmitSuccess();
             toast.success(isEditing ? 'Review updated successfully!' : 'Review added successfully!');
+           
+            
         } catch (error) {
-            console.error('Error submitting review', error);
-            if (error.response) {
-                // The request was made and the server responded with a status code
-                // that falls out of the range of 2xx
-                console.error('Error data:', error.response.data);
-                console.error('Error status:', error.response.status);
-                console.error('Error headers:', error.response.headers);
-                toast.error(`Error submitting review: ${error.response.status}`);
-            } else if (error.request) {
-                // The request was made but no response was received
-                console.error('Error request:', error.request);
-                toast.error("Error submitting review: No response received.");
-            } else {
-                // Something happened in setting up the request that triggered an Error
-                console.error('Error message:', error.message);
-                toast.error(`Error submitting review: ${error.message}`);
-            }
+            console.error('Error submitting review', error.response);
+            toast.error("Error submitting review.");
         }
     };
+
     
 
         return (
@@ -80,6 +47,11 @@ const ReviewForm = ({ volumeId, existingReview, triggerRefresh, hideForm, onSubm
                 <StarRating rating={rating} setRating={setRating} className="mb-4" />
                 <textarea value={comment} onChange={(e) => setComment(e.target.value)} placeholder={ 'Add your review here'} className="w-full p-2 border border-gray-300 rounded mb-4"></textarea>
                 <button type='submit' className="w-full bg-custom-crimson 500 hover:bg-custom-beige 700 text-white font-bold py-2 px-4 rounded">{isEditing ? 'Update Review' : 'Submit Review'}</button>
+                {isEditing && (
+                    <button type='button' onClick={cancelEditing} className="bg-gray-300 text-black font-bold py-2 px-4 rounded">
+                        Cancel
+                    </button>
+                )}
             </form>
         );
 
